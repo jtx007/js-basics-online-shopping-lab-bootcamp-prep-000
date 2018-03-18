6aafc47c0e4049f212030e950bc052522e2def68
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-18 03:48:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function addToCart(item) {
 var price = Math.floor(Math.random() * 100)
 cart.push({itemName: item, itemPrice: price})
 return `${item} has been added to your cart.`
+
 }
 
 function viewCart() {
