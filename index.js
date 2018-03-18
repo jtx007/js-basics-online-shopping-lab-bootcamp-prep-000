0de67209f23d55ae499371d976b0303903b9455b
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-18 03:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
 var price = Math.floor(Math.random() * 100)
 
-cart.push({itemName: item, itemPrice: price} )
+cart.push({itemName: item, itemPrice: price})
 
 }
 
